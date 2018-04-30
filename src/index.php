dcffd53cc6787d29569d0fe8f10a61c78e081dee
test <>
2018-04-30 23:09:19 +0900

    no message

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<html>
+<h1>Quiz</h1>
+<head>
+  <meta charset="UTF-8">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.3/jquery.min.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script type="text/javascript" src="../js/construct.js"></script>
+    <script type="text/javascript" src="../js/quizcontroller.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/quiz.css">
+</head>
+<body>
+<div class="quiz-container">
+  <div id="quiz"></div>
+</div>
+</body>
+</html>
